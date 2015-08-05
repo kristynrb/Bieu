0ddbb85e298fd16b2f742327c52e0d80a8aaea0e
Kristyn Bryan <kristynrb@gmail.com>
2015-08-05 10:29:09 -0400

    updated merge conflicts in js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,20 +2,15 @@
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-<<<<<<< HEAD
+
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-=======
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
->>>>>>> d32a3bd85f2fe74fc25b17260fb76eea04ada3d8
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file.
 //
-<<<<<<< HEAD
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-=======
+
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
->>>>>>> d32a3bd85f2fe74fc25b17260fb76eea04ada3d8
+
 // about supported directives.
 //
 //= require jquery
